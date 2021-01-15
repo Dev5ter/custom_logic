712a49327c756316cdfa1320eba7eb0abb26003a
Dev5ter <42391191+Dev5ter@users.noreply.github.com>
2021-01-15 03:44:59 -0500

    Add files via upload

---
+++ prog_shields/wwr_ui/build_ui.py
@@ -0,0 +1,15 @@
+
+from subprocess import call
+import glob
+import os
+
+for input_path in glob.glob('*.ui'):
+  base_name = os.path.splitext(input_path)[0]
+  output_path = "ui_%s.py" % base_name
+  
+  command = [
+    "pyside2-uic",
+    input_path,
+    "-o", output_path
+  ]
+  result = call(command)
